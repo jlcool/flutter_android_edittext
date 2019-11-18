40ad0baa4d60e6aa017055fc93052cb6412ca4b8
jlcool <jiangliang.net@gmail.com>
2019-11-18 14:30:24 +0800

    Initial commit

---
+++ ios/Classes/FlutterAndroidEdittextPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterAndroidEdittextPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterAndroidEdittextPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterAndroidEdittextPlugin.h"
+
+@implementation FlutterAndroidEdittextPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_android_edittext"
+            binaryMessenger:[registrar messenger]];
+  FlutterAndroidEdittextPlugin* instance = [[FlutterAndroidEdittextPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
